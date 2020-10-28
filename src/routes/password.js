495a99f0391f799f3d9f0e50dc342dcddaec47df
Coty Condry <ccondry@cisco.com>
2020-10-28 05:48:02 -0500

    add test code for resetting password and get/set provision

--- src/routes/password.js
+++ src/routes/password.js
@@ -1,47 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const db = require('../models/db')
 
-// get provision status
-router.get('/', async function (req, res) {
-  try {
-    const username = req.user.username
-    const userId = req.user.id
-    // get provision info from local db, matching either username or userId
-    const query = { $or: [ {username}, {userId} ] }
-    const projection = { _id: 0 }
-    const existing = await db.findOne('toolbox', 'provision', query, {projection})
-    const body = existing || {}
-    // return OK with body
-    return res.status(200).send(body)
-  } catch (e) {
-    // error during processing
-    console.log('failed to get provision status for', req.user.username, `(${req.user.id}):`, e.message)
-    return res.status(500).send({message: e.message})
-  }
-})
-
-// provision user
+// reset password
 router.post('/', async function (req, res) {
   try {
-    const username = req.user.username
-    const userId = req.user.id
-    const filter = { $or: [ {username}, {userId} ] }
-    const data = {
-      username,
-      userId,
-      vertical: 'travel'
-    }
-    const results = await db.upsert('toolbox', 'provision', filter, data)
-    if (results.ok === 1) {
-      // return OK
-      return res.status(200).send()
-    } else {
-      return res.status(500).send(results)
-    }
+    // TODO
+    return res.status(200).send()
   } catch (e) {
-    // error during processing
-    console.log('failed to get provision status for', req.user.username, `(${req.user.id}):`, e.message)
+    console.log('failed to reset password for', req.user.username, `(${req.user.id}):`, e.message)
     return res.status(500).send({message: e.message})
   }
 })
