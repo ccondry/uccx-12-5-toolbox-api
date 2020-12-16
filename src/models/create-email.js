188afc4f81c521317b4785040aff6cb87d37ba36
Coty Condry <ccondry@cisco.com>
2020-12-16 09:25:00 -0600

    don't try to provision email linux account when running in development

--- src/models/create-email.js
+++ src/models/create-email.js
@@ -2,6 +2,11 @@ const exec = require('child_process').exec
 
 function p (id) {
   return new Promise(function (resolve, reject) {
+    // don't perform this in dev
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('skipping create linux user account for email - process.env.NODE_ENV is not "production"')
+      resolve()
+    }
     const command = `/usr/sbin/useradd -s /bin/false -G mail -m -N -p "$(mkpasswd --method=sha-512 'C1sco12345')" ${id}`
     exec(command, function (err, stdout, stderr) {
       if (err) reject(err)
