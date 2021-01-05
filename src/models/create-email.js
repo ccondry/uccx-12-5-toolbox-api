c6a7d89065560ee9267dbad6f78515c01654e501
Coty Condry <ccondry@cisco.com>
2021-01-05 13:26:14 -0600

    add sudo in front of command to create email in linux shell. not sure how this ever worked before.

--- src/models/create-email.js
+++ src/models/create-email.js
@@ -7,7 +7,7 @@ function p (id) {
       console.log('skipping create linux user account for email - process.env.NODE_ENV is not "production"')
       resolve()
     }
-    const command = `/usr/sbin/useradd -s /bin/false -G mail -m -N -p "$(mkpasswd --method=sha-512 'C1sco12345')" ${id}`
+    const command = `sudo /usr/sbin/useradd -s /bin/false -G mail -m -N -p "$(mkpasswd --method=sha-512 'C1sco12345')" ${id}`
     exec(command, function (err, stdout, stderr) {
       if (err) reject(err)
       else resolve()
