5ccf96feccb5a13e035fe425f937d689f5bcf596
Coty Condry <ccondry@cisco.com>
2021-01-11 13:48:30 -0600

    add debug logging

--- src/models/deprovision/ldap/delete-demo-admins.js
+++ src/models/deprovision/ldap/delete-demo-admins.js
@@ -6,11 +6,11 @@ module.exports = async function (user) {
   const ldapUsers = await ldap.listUsers({
     searchDn: process.env.LDAP_LAB_USERS_DN
   })
-  // console.log('successfully got', ldapUsers.length, 'ldap users in', process.env.LDAP_LAB_USERS_DN)
+  console.log('successfully got', ldapUsers.length, 'ldap users in', process.env.LDAP_LAB_USERS_DN)
 
   // find the ldap account matching input user
   const users = ldapUsers.filter(u => u.sAMAccountName === user.username)
-
+  console.log('deleting ldap vpn users:', users)
   const {success, failed} = await ldap.deleteUsers(users)
   // return results
   return {
