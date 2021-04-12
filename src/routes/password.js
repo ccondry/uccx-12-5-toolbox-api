4f5d48c8690fa1479d49df044912e2a52620d912
Coty Condry <ccondry@cisco.com>
2021-04-12 00:07:20 -0500

    implement the password reset route

--- src/routes/password.js
+++ src/routes/password.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const ldap = require('../models/ldap')
 
 // reset password
 router.post('/', async function (req, res) {
   try {
-    // TODO
+    // set user VPN password in LDAP
+    await ldap.resetPassword({
+      adminDn: process.env.LDAP_ADMIN_DN,
+      adminPassword: process.env.LDAP_ADMIN_PASSWORD,
+      newPassword: req.body.password,
+      username: req.user.username
+    })
     return res.status(200).send()
   } catch (e) {
     console.log('failed to reset password for', req.user.username, `(${req.user.id}):`, e.message)
