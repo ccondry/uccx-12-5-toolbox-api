b4792bbf15245c1e67415787566f839471f97b15
Coty Condry <ccondry@cisco.com>
2021-04-28 11:39:42 -0500

    fix cisco username hashes

--- src/models/get-hash.js
+++ src/models/get-hash.js
@@ -2,6 +2,14 @@ const crypto = require('crypto')
 
 // create hash of user sub, and append it to the first 8 characters of the user sub
 function getHash (sub) {
+  // create hash of user sub, and append it to the first 8 characters of the user sub
+  // for cisco.com email addresses, just use the part before the @
+  const parts = email.split('@')
+  if (parts[1] === 'cisco.com') {
+    return parts[0]
+  }
+  // for all other non-cisco users, generate a hash based on their email
+  // with a max length of 14 characters
   const hash = crypto
   .createHash('shake128', {outputLength: 6})
   .update(sub, 'utf-8')
