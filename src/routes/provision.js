07425c44edaaf8018145d3e8d54ee222c1918e36
Coty Condry <ccondry@cisco.com>
2021-06-28 09:47:02 -0500

    add user.email to the projection for deprovisioning users

--- src/routes/provision.js
+++ src/routes/provision.js
@@ -12,7 +12,7 @@ async function checkMaxProvision () {
   try {
     const maxUsers = parseInt(process.env.MAX_USERS || 50)
     console.log(`checking max users provisioned is not exceeding ${maxUsers}`)
-    const projection = {_id: 1, created: 1, modified: 1, userId: 1, username: 1, lastAccess: 1}
+    const projection = {_id: 1, created: 1, modified: 1, userId: 1, username: 1, lastAccess: 1, email: 1}
     // this will sort records with [0] being oldest and [length - 1] being newest
     const sort = {lastAccess: 1}
     const existingUsers = await db.find('toolbox', 'user.provision', {}, projection, sort)
